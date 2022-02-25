1d5a4823ec9777b510c2046ff9d672a87e9fb94d
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-02-25 07:30:04 +0300

    Kotlin can do that!

--- DiscordBot/src/main/kotlin/dev/fstudio/mc_discord_bot/Localization.kt
+++ DiscordBot/src/main/kotlin/dev/fstudio/mc_discord_bot/Localization.kt
@@ -2,41 +2,50 @@ package dev.fstudio.mc_discord_bot
 
 import dev.fstudio.mc_discord_bot.utils.config.ConfigManager.config
 import java.util.*
+import kotlin.reflect.KProperty
 
 var bundle: ResourceBundle = ResourceBundle.getBundle("lang", Locale(config.discord.botLocale))
 
+class LocaleMessage {
+    var value: String? = null
+
+    operator fun getValue(th: Any?, prop: KProperty<*>): String {
+        return value ?: bundle.getString(prop.name).apply { value = this }
+    }
+}
+
 /*   Messages   */
 val footerText: String = bundle.getString("embedFooterText")
-val onlineRequestError: String = bundle.getString("onlineRequestError")
-val statsRequestError: String = bundle.getString("statsRequestError")
+val onlineRequestError by LocaleMessage()
+val statsRequestError by LocaleMessage()
 
 /*   All players   */
-val allPlayersTitle: String = bundle.getString("allPlayersTitle")
+val allPlayersTitle by LocaleMessage()
 
 /*   Top players   */
-val topPlayersTitle: String = bundle.getString("topPlayersTitle")
+val topPlayersTitle by LocaleMessage()
 
 /*   Online players   */
-val onlinePlayersTitle: String = bundle.getString("onlinePlayersTitle")
-val modsCountOnServer: String = bundle.getString("modsCountOnServer")
-val serverVersion: String = bundle.getString("serverVersion")
-val playersCount: String = bundle.getString("playersCount")
+val onlinePlayersTitle by LocaleMessage()
+val modsCountOnServer by LocaleMessage()
+val serverVersion by LocaleMessage()
+val playersCount by LocaleMessage()
 
 /*   Player stats   */
-val realDaysInDay: String = bundle.getString("realDaysInDay")
-val playerStatsTitle: String = bundle.getString("playerStatsTitle")
-val numberOfActions: String = bundle.getString("numberOfActions")
-val killsAndDeath: String = bundle.getString("killsAndDeath")
-val enchantedItems: String = bundle.getString("enchantedItems")
-val droppedItems: String = bundle.getString("droppedItems")
-val killedMobs: String = bundle.getString("killedMobs")
-val jumpCount: String = bundle.getString("jumpCount")
-val walkedDistance: String = bundle.getString("walkedDistance")
-val swamDistance: String = bundle.getString("swamDistance")
-val flownDistance: String = bundle.getString("flownDistance")
-val totalDistance: String = bundle.getString("totalDistance")
+val realDaysInDay by LocaleMessage()
+val playerStatsTitle by LocaleMessage()
+val numberOfActions by LocaleMessage()
+val killsAndDeath by LocaleMessage()
+val enchantedItems by LocaleMessage()
+val droppedItems by LocaleMessage()
+val killedMobs by LocaleMessage()
+val jumpCount by LocaleMessage()
+val walkedDistance by LocaleMessage()
+val swamDistance by LocaleMessage()
+val flownDistance by LocaleMessage()
+val totalDistance by LocaleMessage()
 
 /*   Time   */
-val day: String = bundle.getString("day")
-val hour: String = bundle.getString("hour")
-val minute: String = bundle.getString("minute")
+val day by LocaleMessage()
+val hour by LocaleMessage()
+val minute by LocaleMessage()
